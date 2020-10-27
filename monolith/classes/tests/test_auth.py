80cc3a018c48d8478a8f2926d78414c9517a95ea
mcdeldams <mcsx97@gmail.com>
2020-10-27 17:00:04 +0100

    Updated tests to pass

--- monolith/classes/tests/test_auth.py
+++
@@ -1,5 +0,0 @@
-def test_login(self):
-    pass
-        #app = create_app()
-        #with app.app_context():
-            # using admin credentials to test the login, should a test user be in db instead?
